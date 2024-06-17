123e5d3751b47908e54104f012b8187a00209349
Александр Кинцлер <alexander.kintsler@yandex.ru>
2024-06-17 10:47:47 +0300

    removeRoom func and logs

--- go/packages/webrtc_signaling/rooms.go
+++ go/packages/webrtc_signaling/rooms.go
@@ -2,7 +2,6 @@ package webrtc_signaling
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
@@ -26,11 +25,22 @@ func getRoomID(device int) int {
 func RoomHandler(rooms map[int]ReceiveChannels, mu *sync.Mutex) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// получение девайса из запроса
-		userDevice, _ := strconv.Atoi(r.URL.Query().Get("device"))
-		roomID := getRoomID(userDevice) // как будто ид комнаты из БД получили
+		userDevice, err := strconv.Atoi(r.URL.Query().Get("device"))
+
+		// Валидация девайса
+		if err != nil {
+			log.Println("no correct device", r.URL.Query().Get("device"))
+			return
+		}
+
+		// как будто ид комнаты из БД получили
+		roomID := getRoomID(userDevice)
+		// если roomID не будет найден в БД, сделать return, вернуть response ошибку
+
 		// обеспечение потокобезопасности
 		mu.Lock()
 		defer mu.Unlock()
+
 		// Если пользователь уже есть в комнате, не создаем новое подключение
 		if _, ok := rooms[roomID][userDevice]; ok {
 			jsonResponse, _ := json.Marshal(struct {
@@ -39,44 +49,59 @@ func RoomHandler(rooms map[int]ReceiveChannels, mu *sync.Mutex) http.HandlerFunc
 			if _, err := w.Write(jsonResponse); err != nil {
 				log.Println(err)
 			}
+			log.Println("error: OTHER_DEVICE")
 			return
 		}
+
 		// Если комната на сервере еще не создана (первое подключение), создаем ее.
 		if _, ok := rooms[roomID]; !ok {
 			rooms[roomID] = make(ReceiveChannels)
+			log.Println("room", roomID, "was created")
 		}
+
 		// Установка вебсокет-соединения
-		conn, _ := upgrader.Upgrade(w, r, nil)
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
 		// Создания канала для доставку сообщения вебсокет-соединению
 		userChannel := make(chan []byte)
+
 		// Привязка канала юзера к конкретной комнате
 		rooms[roomID][userDevice] = userChannel
-
 		log.Println("Device", userDevice, "connected to room", roomID)
 
-		go readMessagesFromWebsocket(conn, rooms[roomID], userDevice, mu) // from webSocket
-		go writeMessagesToWebsocket(conn, rooms[roomID], userDevice, mu)
+		// Запуск горутин на слушание и отправку сообщений
+		go readMessagesFromWebsocket(conn, rooms[roomID], userDevice, mu, rooms, roomID) // from webSocket
+		go writeMessagesToWebsocket(conn, rooms[roomID], userDevice, mu, rooms, roomID)
 	}
 }
 
-func readMessagesFromWebsocket(conn *websocket.Conn, channels ReceiveChannels, userDevice int, mu *sync.Mutex) {
+func readMessagesFromWebsocket(conn *websocket.Conn, channels ReceiveChannels, userDevice int, mu *sync.Mutex, rooms map[int]ReceiveChannels, roomID int) {
 	// Отложенный вызов функции, закрывающей вебсокет-соединение и удаляющей юзера из комнаты.
-	defer removeDeviceFromRoom(channels, userDevice, mu, conn)
+	defer func() {
+		removeDeviceFromRoom(channels, userDevice, mu, conn)
+		removeRoomIfNoConnections(rooms, roomID, mu)
+	}()
 
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			return // разорвано соединение, конец горутины
 		}
-		fmt.Println(msg)
 		for _, userChannel := range channels {
 			userChannel <- msg
 		}
 	}
 }
 
-func writeMessagesToWebsocket(conn *websocket.Conn, channels ReceiveChannels, userDevice int, mu *sync.Mutex) {
-	defer removeDeviceFromRoom(channels, userDevice, mu, conn)
+func writeMessagesToWebsocket(conn *websocket.Conn, channels ReceiveChannels, userDevice int, mu *sync.Mutex, rooms map[int]ReceiveChannels, roomID int) {
+	defer func() {
+		removeDeviceFromRoom(channels, userDevice, mu, conn)
+		removeRoomIfNoConnections(rooms, roomID, mu)
+	}()
 
 	// Потокобезопасное чтение мапы
 	mu.Lock()
@@ -112,6 +137,17 @@ func removeDeviceFromRoom(room ReceiveChannels, userDevice int, mu *sync.Mutex, 
 }
 
 // В каком случае удаляем комнату?
-func removeRoom() {
-
+func removeRoomIfNoConnections(rooms map[int]ReceiveChannels, roomID int, mu *sync.Mutex) {
+	// Потокобезопасность
+	mu.Lock()
+	defer mu.Unlock()
+	// проверка, существует ли еще эта комната
+	if _, ok := rooms[roomID]; !ok {
+		return // возврат из функции, если комната была удалена в другой горутине
+	}
+	// Удаление комнаты, если в ней больше не осталось подключенных девайсов
+	if len(rooms[roomID]) == 0 {
+		delete(rooms, roomID)
+		log.Println("room", roomID, "was deleted (no connections)")
+	}
 }
