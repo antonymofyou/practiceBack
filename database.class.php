8393409a0b880351d5f3c77a1d37cf05a910b417
Lambda <anagromy13@yandex.ru>
2024-06-27 21:23:54 +0300

    Метод добавления нового сотрудника в таблицу

---
+++ database.class.php
@@ -0,0 +1,62 @@
+<?php
+
+class Database
+{
+    private $host;
+    private $database;
+    private $user;
+    private $password;
+    private $charset;
+
+    private $dsn;
+    private $opt;
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->host = "127.0.0.1";
+        $this->database = "hr";
+        $this->user = "root";
+        $this->password = "";
+        $this->charset = "utf8mb4";
+
+        $this->dsn = "mysql:host=$this->host;dbname=$this->database;charset=$this->charset";
+        $this->opt = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($this->dsn, $this->user, $this->password, $this->opt);
+        } catch (Exception $ex) {
+            // $this->logger->new('ERROR', 'DATABASE', 'Failed to connect to database ' . str_replace("\n", "_", $ex));
+            die('Failed to connect to database');
+        }
+    }
+
+    public function single_query($sql, $data)
+    {
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($data);
+            return array(
+                $stmt->fetch(PDO::FETCH_ASSOC),
+                $this->pdo->lastInsertId()
+            );
+        } catch (Exception $ex) {
+            die('Unknown error occurred while executing query.');
+        }
+    }
+
+    public function plural_query($sql, $data)
+    {
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($data);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $ex) {
+            die('Unknown error occurred while executing query.');
+        }
+    }
+}
