2198c21d5436d4ed795d243a95420d6b56feb417
fastik1 <47109537+fastik1@users.noreply.github.com>
2024-07-17 00:31:41 +0300

    перенос структур в основной файл

--- go/packages/webrtc_signaling/rooms.go
+++ go/packages/webrtc_signaling/rooms.go
@@ -1,12 +1,17 @@
 package webrtc_signaling
 
 import (
+	"errors"
 	"github.com/gorilla/websocket"
 	"log"
 	"nasotku/includes/api_root_classes"
 	"nasotku/includes/auth"
+	"nasotku/includes/db"
 	"net/http"
+	"sync"
 )
+
+//--------------------------------Классы запросов
 
 // класс запроса
 type WebrtcSignalingRequest struct {
@@ -30,6 +35,42 @@ const (
 	DataTypeFinishSendIceCandidates = "FINISH_SEND_ICE_CANDIDATES"
 )
 
+//--------------------------------Комнаты
+
+// Мапа rooms хранит указатели на rd, так как rd - тяжелая структура, будет затратно каждый раз копировать ее
+// из функции в функцию. Тем более в горутине мы изменяем статус уже созданной rd, поэтому указатель необходим.
+type roomDataStorage struct {
+	sync.Mutex                   // потокобезопасность для rooms
+	rooms      map[int]*roomData // ключ - id комнаты
+}
+
+type roomInfoFromDB struct {
+	ID              int    `sql:"room_id"`
+	initiatorDevice string `sql:"initiator_device"`
+	responderDevice string `sql:"responder_device"`
+}
+
+// Структура для хранения информации о комнате
+type roomData struct {
+	*roomInfoFromDB
+	sync.Mutex
+	initiatorChannel                   chan []byte
+	responderChannel                   chan []byte
+	initiatorConnected                 bool
+	responderConnected                 bool
+	isFinishSendIceCandidatesInitiator bool
+	isFinishSendIceCandidatesResponder bool
+	status                             string
+}
+
+// статусы комнаты
+const (
+	RoomStatusWaitSecondUser    = "WAIT_SECOND_USER"
+	RoomStatusWaitOffer         = "WAIT_OFFER"
+	RoomStatusWaitAnswer        = "WAIT_ANSWER"
+	RoomStatusFinishReceiveData = "FINISH_RECEIVE_DATA"
+)
+
 // хранение информации о комнатах (аллокация сразу под 1000 элементов)
 var rds = roomDataStorage{
 	rooms: make(map[int]*roomData, 1000),
@@ -40,6 +81,8 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+
+//--------------------------------Обработка запроса
 
 func RoomHandler(w http.ResponseWriter, r *http.Request) {
 	in := &api_root_classes.MainRequestClass{}
@@ -291,5 +334,56 @@ func removeAllConnections(conn *websocket.Conn, roomID int, rdStorage *roomDataS
 		rd.closeConnections()
 		delete(rdStorage.rooms, roomID)
 		log.Println("room", roomID, "was deleted")
+	}
+}
+
+// функция получения информации о комнате из БД
+func getRoomByDevice(device string) (*roomInfoFromDB, error) {
+	// получаем сессию пользователя по device (пользователь может быть как инициатором, так и респондером)
+	rows, err := db.Db.Query(
+		"SELECT `id`, `user_initiator_device`, `user_responder_device` FROM `wrtc_sessions` WHERE `user_initiator_device` = ? OR `user_responder_device` = ? LIMIT 1;",
+		device,
+		device,
+	)
+	if err != nil {
+		return nil, errors.New("ошибка БД: " + err.Error())
+	}
+	defer rows.Close()
+
+	if !rows.Next() { // проверяем наличие результата
+		return nil, errors.New("комната для переданного device не найдена")
+	}
+
+	// биндим результат последовательно к каждому полю структуры (для этого передаем указатель на поле)
+	roomInfo := &roomInfoFromDB{}
+	rows.Scan(&roomInfo.ID, &roomInfo.initiatorDevice, &roomInfo.responderDevice)
+
+	return roomInfo, nil
+}
+
+func newRoomData(roomInfo *roomInfoFromDB) *roomData {
+	return &roomData{
+		roomInfoFromDB:   roomInfo,
+		initiatorChannel: make(chan []byte),
+		responderChannel: make(chan []byte),
+	}
+}
+
+// корректное закрытие каналов текущей комнаты
+func (rd *roomData) closeConnections() {
+	rd.Lock()
+	defer rd.Unlock()
+
+	if rd.initiatorConnected {
+		// Если инициатор еще не отключен, отключаем его, ставим флажок
+		close(rd.initiatorChannel)
+		rd.initiatorConnected = false
+		log.Println(rd.initiatorDevice, "disconnected")
+	}
+	if rd.responderConnected {
+		// Если респондер еще не отключен, отключаем его, ставим флажок
+		close(rd.responderChannel)
+		rd.responderConnected = false
+		log.Println(rd.responderDevice, "disconnected")
 	}
 }
