c888e1d08f41928dcb95f5c812801fc3db28c622
Evgenii <GmftG>
2024-07-19 22:43:57 +0400

    get_dz_list fifth commit

--- php/home_tasks/get_dz_list.php
+++ php/home_tasks/get_dz_list.php
@@ -48,15 +48,15 @@ try {
 }
 
 //--------------------------------Проверка пользователя
-require $_SERVER['DOCUMENT_ROOT'] . '/app/api/includes/check_user.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/app/api/includes/check_user.inc.php';
 if (!in_array($user_type, ['Админ', 'Куратор'])) $out->make_wrong_resp('Ошибка доступа');
 
 //--------------------------------Получение домашних заданий не с номером 200
 $stmt = $pdo->prepare("
-        SELECT 'is_probnik', 'ht_number', 'type_p1', 'ht_nums_P1', 'ht_ums_P2', 'ht_deadline', 'ht_deadline_time', 'ht_status'
-        FROM `home_tasks`
-        WHERE `ht_number` != :ht_number
-        ORDER BY `ht_deadline`"
+        SELECT is_probnik, ht_number, type_p1, ht_nums_P1, ht_nums_P2, ht_deadline, ht_deadline_time, ht_status
+        FROM home_tasks
+        WHERE ht_number != :ht_number
+        ORDER BY ht_deadline"
         ) or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (1)');
 
 $htNumber = 200;
@@ -65,23 +65,26 @@ $stmt->execute(['ht_number' => $htNumber]);
 if ($stmt->rowCount() == 0) $out->make_wrong_resp("записей не найдено в home_tasks");
 $notNumber200Tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $stmt->closeCursor(); unset($stmt);
-$notNumber200Tasks = [
-    'isProbnik' => (string) $notNumber200Tasks['is_probnik'],
-    'htNumber' => (string) $notNumber200Tasks['ht_number'],
-    'typeP1' => (string) $notNumber200Tasks['type_p1'],
-    'htNumsP1' => (string) $notNumber200Tasks['ht_nums_P1'],
-    'htNumsP2' => (string) $notNumber200Tasks['ht_nums_P2'],
-    'htDeadline' => (string) $notNumber200Tasks['ht_deadline'],
-    'htDeadlineTime' => (string) $notNumber200Tasks['ht_deadline_time'],
-    'htStatus' => (string) $notNumber200Tasks['ht_status'],
-];
+foreach ($notNumber200Tasks as $row){
+    $tasks = [
+        'isProbnik' => (string) $row['is_probnik'],
+        'htNumber' => (string) $row['ht_number'],
+        'typeP1' => (string) $row['type_p1'],
+        'htNumsP1' => (string) $row['ht_nums_P1'],
+        'htNumsP2' => (string) $row['ht_nums_P2'],
+        'htDeadline' => (string) $row['ht_deadline'],
+        'htDeadlineTime' => (string) $row['ht_deadline_time'],
+        'htStatus' => (string) $row['ht_status'],
+    ];
+    $out-> homeTasks[] = $tasks;
+}
 
 //--------------------------------Получение домашних заданий с номером 200
 $stmt = $pdo->prepare("
-        SELECT 'is_probnik', 'ht_number', 'type_p1', 'ht_nums_P1', 'ht_ums_P2', 'ht_deadline', 'ht_deadline_time', 'ht_status'
-        FROM `home_tasks`
-        WHERE `ht_number` = :ht_number
-        ORDER BY `ht_deadline`"
+        SELECT is_probnik, ht_number, type_p1, ht_nums_P1, ht_nums_P2, ht_deadline, ht_deadline_time, ht_status
+        FROM home_tasks
+        WHERE ht_number = :ht_number
+        ORDER BY ht_deadline"
         ) or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (1)');
 
 $htNumber = 200;
@@ -90,32 +93,32 @@ $stmt->execute(['ht_number' => $htNumber]);
 if ($stmt->rowCount() == 0) $out->make_wrong_resp("записей не найдено в home_tasks");
 $number200Tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $stmt->closeCursor(); unset($stmt);
-$number200Tasks = [
-    'isProbnik' => (string) $number200Tasks['is_probnik'],
-    'htNumber' => (string) $number200Tasks['ht_number'],
-    'typeP1' => (string) $number200Tasks['type_p1'],
-    'htNumsP1' => (string) $number200Tasks['ht_nums_P1'],
-    'htNumsP2' => (string) $number200Tasks['ht_nums_P2'],
-    'htDeadline' => (string) $number200Tasks['ht_deadline'],
-    'htDeadlineTime' => (string) $number200Tasks['ht_deadline_time'],
-    'htStatus' => (string) $number200Tasks['ht_status'],
-];
-
-//--------------------------------Записываем полученные задачи в $tasks
-$homeTasks = array_merge($number200Tasks, $notNumber200Tasks);
+foreach ($number200Tasks as $row){
+    $tasks = [
+        'isProbnik' => (string) $row['is_probnik'],
+        'htNumber' => (string) $row['ht_number'],
+        'typeP1' => (string) $row['type_p1'],
+        'htNumsP1' => (string) $row['ht_nums_P1'],
+        'htNumsP2' => (string) $row['ht_nums_P2'],
+        'htDeadline' => (string) $row['ht_deadline'],
+        'htDeadlineTime' => (string) $row['ht_deadline_time'],
+        'htStatus' => (string) $row['ht_status'],
+    ];
+    $out-> homeTasks[] = $tasks;
+}
 
 //--------------------------------Формируем выдачу для перекрестной проверки
 $stmt = $pdo->prepare("
-    SELECT `cross_check`.`ht_num`, `cross_check`.`ht_status`, `cross_check`.`cc_check_date`, `cross_check`.`cc_check_time`,
-    `ht_user`.`ht_user_checker`, `ht_user`.`ht_number`
-    FROM `cross_check`
-    LEFT JOIN `ht_user` ON `cross_check`.`curator_vk_id` = `ht_user`.`ht_user_checker`
-    AND `cross_check`.`ht_num` = `ht_user`.`ht_number` AND `ht_user`.`ht_user_status_p2` = 'Проверен'
-    LEFT JOIN `home_tasks` ON `home_tasks`.`ht_number` = `cross_check`.`ht_num`
-    WHERE `cross_check`.`checker_id` = :user_id
-    AND (`cross_check`.`ht_status` = 0 OR `cross_check`.`ht_status` IS NULL)
-    AND DATEDIFF(CURDATE(), `home_tasks`.`ht_deadline`) > -3
-    GROUP BY `cross_check`.`ht_num`, `cross_check`.`checker_id`"
+    SELECT cross_check.ht_num, cross_check.ht_status, cross_check.cc_check_date, cross_check.cc_check_time,
+    ht_user.ht_user_checker, ht_user.ht_number
+    FROM cross_check
+    LEFT JOIN ht_user ON cross_check.curator_vk_id = ht_user.ht_user_checker
+    AND cross_check.ht_num = ht_user.ht_number AND ht_user.ht_user_status_p2 = 'Проверен'
+    LEFT JOIN home_tasks ON home_tasks.ht_number` = cross_check.ht_num
+    WHERE cross_check.checker_id = :user_id
+    AND (cross_check.ht_status = 0 OR cross_check.ht_status IS NULL)
+    AND DATEDIFF(CURDATE(), home_tasks.ht_deadline) > -3
+    GROUP BY cross_check.ht_num, cross_check.checker_id"
     ) or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (1)');
 
 $stmt->execute(['user_id' => $user_vk_id]);
@@ -123,16 +126,17 @@ $stmt->execute(['user_id' => $user_vk_id]);
 if ($stmt->rowCount() == 0) $out->make_wrong_resp("записей не найдено");
 $crossChecks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $stmt->closeCursor(); unset($stmt);
-$crossChecks = [
-    'ccCheckDate' => (string) $crossCheks['cc_check_date'],
-    'ccCheckTime' => (string) $crossCheks['cc_check_time'],
-    'htStatus' => (string) $crossCheks['ht_status'],
-    'htNumber' => (string) $crossCheks['ht_number'],
-    'htNum' => (string) $crossCheks['ht_num'],
-];
+foreach ($crossChecks as $row){
+    $checks = [
+        'ccCheckDate' => (string) $row['cc_check_date'],
+        'ccCheckTime' => (string) $row['cc_check_time'],
+        'htStatus' => (string) $row['ht_status'],
+        'htNumber' => (string) $row['ht_number'],
+        'htNum' => (string) $row['ht_num'],
+    ];
+    $out-> crossChecks[] = $checks;
+}
 
 //--------------------------------Формируем ответ
 $out->success = '1';
-$out->homeTasks = $homeTasks;
-$out->crossCheks = $crossCheks;
 $out->make_resp('');
