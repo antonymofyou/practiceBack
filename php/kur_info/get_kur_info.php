fd91a20f6030934c923b50b356c36a1232c6576d
Egor Vaschenko <vasch3nko.3gor@gmail.com>
2024-07-26 20:00:40 +0500

    Update get_kur_info.php

--- php/kur_info/get_kur_info.php
+++ php/kur_info/get_kur_info.php
@@ -6,16 +6,18 @@ require $_SERVER['DOCUMENT_ROOT'] . '/app/api/includes/config_api.inc.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/app/api/includes/root_classes.inc.php';
 
 // Класс запроса
-class GetInfoByPageRequest extends MainRequestClass {
-    public $page   = '';  // Страница "1" - информация для кураторов, страница "2" - скрипты диалогов
-    public $mode   = '';  // Режим получения данных (доступные значения: "view": для просмотра; "change": для изменения, доступ только админу)
+class GetInfoByPageRequest extends MainRequestClass
+{
+    public $page = '';  // Страница "1" - информация для кураторов, страница "2" - скрипты диалогов
+    public $mode = '';  // Режим получения данных (доступные значения: "view": для просмотра; "change": для изменения, доступ только админу)
 }
 
 $in = new GetInfoByPageRequest();
 $in->from_json(file_get_contents('php://input'));
 
 // Класс ответа
-class GetInfoByPageResponse extends MainResponseClass {
+class GetInfoByPageResponse extends MainResponseClass
+{
     public $infos = []; /* Массив словарей данных информации для кураторов, где каждый словарь имеет следующие поля:
     - id          - Id информации для кураторов
     - header      - Заголовок
@@ -29,15 +31,16 @@ class GetInfoByPageResponse extends MainResponseClass {
 $out = new GetInfoByPageResponse();
 
 // Валидация $in->page
-if (((string) (int) $in->page) !== ((string) $in->page) || (int) $in->page <= 0) $out->make_wrong_resp('Параметр {page} задан некорректно или отсутствует');
+if (((string)(int)$in->page) !== ((string)$in->page) || (int)$in->page <= 0) $out->make_wrong_resp('Параметр {page} задан некорректно или отсутствует');
 
 // Валидация $in->mode
-if (!in_array($in->mode, ['view','change'])) $out->make_wrong_resp('Параметр {mode} задан некорректно или отсутствует');
+if (!in_array($in->mode, ['view', 'change'])) $out->make_wrong_resp('Параметр {mode} задан некорректно или отсутствует');
 
 // Проверка пользователя
 require $_SERVER['DOCUMENT_ROOT'] . '/app/api/includes/check_user.inc.php';
 if (!in_array($user_type, ['Куратор', 'Админ'])) $out->make_wrong_resp('Ошибка доступа');
 
+// Проверка доступа к режиму редактирования
 if ($in->mode == 'change' && $user_type != 'Админ') $out->make_wrong_resp('Ошибка доступа. Режим редактирования доступен только администратору');
 
 // Подключение к БД
@@ -53,50 +56,61 @@ try {
 
 // Получаем информацию для кураторов
 // Режим "view"
-if($in->mode == 'view') {
-    $stmt = $pdo->prepare("SELECT `id`, `header`, `body`, `when_changed`, `who_changed` FROM `info` WHERE `page` = :infoPage AND `public` = '1'")
+if ($in->mode == 'view') {
+    // Подготовка запроса для получения записи
+    $stmt = $pdo->prepare("SELECT `id`, `header`, `body`, `when_changed`, `who_changed` FROM `info` WHERE `page` = :page AND `public` = '1'")
     or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (1)');
-    $stmt->execute(['infoPage' => $in->page]) or $out->make_wrong_resp('Ошибка базы данных: выполнение запроса (1)');
 
+    // Выполнение запроса для получения записи
+    $stmt->execute(['page' => $in->page]) or $out->make_wrong_resp('Ошибка базы данных: выполнение запроса (1)');
+
+    // Проверка наличия записей
     if ($stmt->rowCount() == 0) $out->make_wrong_resp("Информация для кураторов на странице {$in->page} не найдена");
 
     $infos = [];
     while ($info = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $infos[] = [
-            'id'          => (string) $info['id'],
-            'header'      => (string) $info['header'],
-            'body'        => (string) $info['body'],
-            'whenChanged' => (string) $info['when_changed'],
-            'whoChanged'  => (string) $info['who_changed'],
+            'id' => (string)$info['id'],
+            'header' => (string)$info['header'],
+            'body' => (string)$info['body'],
+            'whenChanged' => (string)$info['when_changed'],
+            'whoChanged' => (string)$info['who_changed'],
         ];
     }
 
-    $stmt->closeCursor(); unset($stmt);
+    $stmt->closeCursor();
+    unset($stmt);
 }
 
 // Режим "change"
-if($in->mode == "change") {
-    $stmt = $pdo->prepare("SELECT `id`, `header`, `body`, `public` FROM `info` WHERE `page` = :infoPage") 
+if ($in->mode == "change") {
+    // Подготовка запроса для получения записи
+    $stmt = $pdo->prepare("SELECT `id`, `header`, `body`, `when_changed`, `who_changed`, `public` FROM `info` WHERE `page` = :page")
     or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (2)');
-    
-    $stmt->execute(['infoPage' => $in->page]) or $out->make_wrong_resp('Ошибка базы данных: выполнение запроса (2)');
 
+    // Выполнение запроса для получения записи
+    $stmt->execute(['page' => $in->page]) or $out->make_wrong_resp('Ошибка базы данных: выполнение запроса (2)');
+
+    // Проверка наличия записей
     if ($stmt->rowCount() == 0) $out->make_wrong_resp("Информация для кураторов на странице {$in->page} не найдена");
 
     $infos = [];
     while ($info = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $infos[] = [
-            'id'     => (string) $info['id'],
-            'header' => (string) $info['header'],
-            'body'   => (string) $info['body'],
-            'public' => (string) $info['public'],
+            'id' => (string)$info['id'],
+            'header' => (string)$info['header'],
+            'body' => (string)$info['body'],
+            'whenChanged' => (string)$info['when_changed'],
+            'whoChanged' => (string)$info['who_changed'],
+            'public' => (string)$info['public'],
         ];
     }
 
-    $stmt->closeCursor(); unset($stmt);
+    $stmt->closeCursor();
+    unset($stmt);
 }
 
-// Формируем ответ
+// Ответ
 $out->success = '1';
-$out->infos = (object) $infos;
+$out->infos = $infos;
 $out->make_resp('');
