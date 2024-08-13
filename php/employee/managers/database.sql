1b7318a740fc136e549d67f13625025a2934b530
Garodil <garodidail@gmail.com>
2024-08-13 19:53:56 +0500

    меняем таблицы базы данных + переименования

--- php/employee/managers/database.sql
+++ php/employee/managers/database.sql
@@ -1,19 +1,18 @@
-CREATE TABLE `staff` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `vk_id` INT NOT NULL,
-    `type` VARCHAR(255) NOT NULL,
-    `first_name` VARCHAR(255) NOT NULL,
-    `last_name` VARCHAR(255) NOT NULL,
-    `middle_name` VARCHAR(255) NOT NULL,
-    `blocked`     BOOLEAN NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`)
-) ENGINE=innodb DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `managers` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_vk_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  INDEX `user_vk_id` (`user_vk_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `staff_pers_data` (
-    `staff_id` INT NOT NULL,
+CREATE TABLE `manager_pers_data` (
+    `manager_id` INT NOT NULL,
     `field`  VARCHAR(255) NOT NULL,
     `value` TEXT NULL,
     `comment` TEXT NULL,
-    PRIMARY KEY (`staff_id`, `field`),
-    INDEX (`staff_id`)
+    PRIMARY KEY (`manager_id`, `field`),
+    INDEX (`manager_id`)
 ) ENGINE=innodb DEFAULT CHARSET=utf8mb4;
