edd06533b7b397864e29714df416a21f7e35a3e8
blaspin <daniilschmakov@yandex.ru>
2024-09-01 01:50:34 +0500

    rewrote get and create crm methods

---
+++ php/crm/get_crm_comments.php
@@ -0,0 +1,112 @@
+<?php // Получение комментариев по ученику
+
+header('Content-Type: application/json; charset=utf-8');
+
+require $_SERVER['DOCUMENT_ROOT'] . 'app/api/includes/config_api.inc.php';
+require $_SERVER['DOCUMENT_ROOT'] . 'app/api/includes/root_classes.inc.php';
+
+// класс запроса
+class CrmGetComment extends MainRequestClass
+{
+    public $userVkId = ''; // ID пользователя, которому нужно вывести комментарии
+}
+
+$in = new CrmGetComment();
+$in->from_json(file_get_contents('php://input'));
+
+// класс ответа
+class CrmGetCommentResponce extends MainResponseClass
+{
+    /*
+     * Массив словарей, где каждый словарь содержит следующие поля:
+     *  - comment - комментарий
+     *  - date - дата создания в формате yyyy-mm-dd
+     *  - time - время создания в формате hh-mm-ss
+     *  - editor - id создателя комментария
+     *  - editorName - имя создателя комментария
+     *  - editorSurname - фамилия создателя комментария
+     */
+    public $comments = [];
+}
+
+$out = new CrmGetCommentResponce();
+
+//--------------------------------Подключение к базе данных
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE_HR . ";charset=" . DB_CHARSET, DB_USER, DB_PASSWORD, DB_SSL_FLAG === MYSQLI_CLIENT_SSL ? [
+        PDO::MYSQL_ATTR_SSL_CA => DB_SSL_CA,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+    ] : [PDO::MYSQL_ATTR_MULTI_STATEMENTS => false]);
+} catch (PDOException $exception) {
+    $out->make_wrong_resp('Нет соединения с базой данных');
+}
+
+//--------------------------------Проверка пользователя
+require $_SERVER['DOCUMENT_ROOT'] . 'app/api/includes/check_user.inc.php';
+if (!(in_array($user_type, ['Админ', 'Куратор']))) $out->make_wrong_resp('Нет доступа');
+
+//--------------------------------Валидация $in->userVkId
+if (((string) (int) $in->userVkId) !== ((string) $in->userVkId) || (int) $in->userVkId <= 0) $out->make_wrong_resp("Параметр 'userVkId' задан некорректно или отсутствует");
+
+$curatorId = ""; // Содержит куратора пользователя с id $in->userVkId
+
+$stmt = $pdo->prepare("
+    SELECT `users`.`user_curator`
+    FROM `users`
+    WHERE `users`.`user_vk_id` = :user_vk_id;
+") or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (1)');
+$stmt->execute([
+    'user_vk_id' => $in->userVkId
+]) or $out->make_wrong_resp('Ошибка базы данных: выполнение запроса (1)');
+
+if ($stmt->rowCount() === 0) $out->make_wrong_resp("Не найдено ни одного пользователя с 'userVkId' = {$in->userVkId} ");
+
+$studentData = mysqli_fetch_assoc($stmt->fetch(PDO::FETCH_ASSOC));
+
+$curatorId = $studentData['user_curator'];
+if ($curatorId == null || empty($curatorId)) $out->make_wrong_resp("У ученика с 'userVkId' = {$in->userVkId} нет куратора");
+
+//--------------------------------Проверка, смотрит ли ученика тот куратор, который указан в БД
+require $_SERVER['DOCUMENT_ROOT'] . 'app/api/includes/check_user.inc.php';
+if (!($user_type == "Куратор") && ($user_vk_id == $curatorId) && ($user_vk_id != changer_user) && !(in_array($user_type, main_managers))) $out->make_wrong_resp('Это не твой ученик');
+
+//--------------------------------Проверка, смотрит ли пользователь комментарии про себя
+require $_SERVER['DOCUMENT_ROOT'] . 'app/api/includes/check_user.inc.php';
+if (!($user_type != "Админ") && ($user_vk_id == $curatorId)) $out->make_wrong_resp('Нельзя смотреть комментарии про себя');
+
+//--------------------------------Вставка новой записи в crm_comment
+$stmt = $pdo->prepare("
+    SELECT `crm_comments`.`crm_date`, `crm_comments`.`crm_time`, `crm_comments`.`crm_editor`, `crm_comments`.`crm_comment`,
+           `users`.`user_name`, `users`.`user_surname`
+    FROM `crm_comments`
+    LEFT JOIN `users` on `crm_comments`.`crm_editor` = `users`.`user_vk_id`
+    WHERE `crm_comments`.`user_vk_id` = :user_vk_id;
+") or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (2)');
+$stmt->execute([
+    'user_vk_id' => $in->userVkId
+]) or $out->make_wrong_resp('Ошибка базы данных: выполнение запроса (2)');
+/*
+     * Массив словарей, где каждый словарь содержит следующие поля:
+     *  - comment - комментарий
+     *  - date - дата создания в формате yyyy-mm-dd
+     *  - time - время создания в формате hh-mm-ss
+     *  - editor - id создателя комментария
+     *  - editorName - имя создателя комментария
+     *  - editorSurname - фамилия создателя комментария
+     */
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $comments = [
+        'comment' => $row['crm_comments'],
+        'date' => $row['crm_date'],
+        'time' => $row['crm_time'],
+        'editor' => $row['crm_editor'],
+        'editorName' => $row['user_name'],
+        'editorSurname' => $row['user_surname'],
+    ];
+}
+
+//--------------------------------Формирование ответа
+$out->success = '1';
+$out->comments = $comments;
+$out->make_resp('');
