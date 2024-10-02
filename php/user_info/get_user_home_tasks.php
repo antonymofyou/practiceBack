d18872975ac0b74754fd8f775f5157160f25e958
blaspin <daniilschmakov@yandex.ru>
2024-10-02 23:55:57 +0500

    rewrote after review

--- php/user_info/get_user_home_tasks.php
+++ php/user_info/get_user_home_tasks.php
@@ -13,32 +13,26 @@ class UserInfoStinfHometasks extends MainRequestClass
 }
 
 $in = new UserInfoStinfHometasks();
-$in->from_json('{
-    "device": "",
-    "signature": "aaasdasss",
-    "studentId": "1"
- }');
- $user_type = "Админ";
- $user_vk_id = "1";
+
 // класс ответа
 class UserInfoStinfHometasksResponce extends MainResponseClass
 {
     /*
      * Массив словарей, где каждый словарь содержит следующие поля:
      *  - htNum - номер задания
-	 *  - videoId - id видео
+     *  - videoId - id видео
      *  - forOrder - приоритет упорядочивания
      *  - access - доступ к видео
      *  - prosmotrov - количество просмотров
      *  - htStatus - статус домашнего задания
      *  - htUserBallovP1 - количество баллов за задание 1
-	 *  - htUserMaxballovP1 - максимальное количество баллов за задание 1
-	 *  - htUserStatusP1 - статус выполнения задания 1
-	 *  - htUserDateP1 - дата задания 1
+     *  - htUserMaxballovP1 - максимальное количество баллов за задание 1
+     *  - htUserStatusP1 - статус выполнения задания 1
+     *  - htUserDateP1 - дата задания 1
      *  - htUserBallovP2 - количество баллов за задание 2
-	 *  - htUserMaxballovP2 - максимальное количество баллов за задание 2
-	 *  - htUserStatusP2 - статус выполнения задания 2
-	 *  - htUserDateP2 - дата задания 2
+     *  - htUserMaxballovP2 - максимальное количество баллов за задание 2
+     *  - htUserStatusP2 - статус выполнения задания 2
+     *  - htUserDateP2 - дата задания 2
      */
     public $homeTasks = [];
 }
@@ -84,16 +78,16 @@ if($user_type == 'Куратор' && $student['user_curator'] != $user_vk_id && 
 $homeTasks = [];
 
 $stmt = $pdo->prepare("SELECT  `home_tasks`.`ht_number` as `ht_num`, IF(`home_tasks`.`ht_number` = '200', 1, 0) AS `for_order`,
-	`video_access`.`access`, `video_access`.`views`, `home_tasks`.`ht_status` ,  `video_access`.`video_id`,
-	`ht_user`.`ht_user_ballov_p1`, `ht_user`.`ht_user_maxballov_p1`, `ht_user`.`ht_user_status_p1`, `ht_user`.`ht_user_date_p1`, 
-	`ht_user`.`ht_user_ballov_p2`, `ht_user`.`ht_user_maxballov_p2`, `ht_user`.`ht_user_status_p2`, `ht_user`.`ht_user_date_p2` 
-	FROM  `home_tasks`
-		LEFT JOIN `ht_user` on `home_tasks`.`ht_number`=`ht_user`.`ht_number` AND `ht_user`.`user_id` =  :student_id
-		LEFT JOIN `videos` on `videos`.`video_lesson_num`=`home_tasks`.`ht_number`
-		LEFT JOIN `video_access` ON `video_access`.`video_id`=`videos`.`video_id` AND `video_access`.`user_vk_id`= :student_id
-	WHERE  `home_tasks`.`ht_status`
-	IN ('Выполнение',  'Проверка',  'Завершено')
-	ORDER BY  `for_order`, `home_tasks`.`ht_number` DESC;
+    `video_access`.`access`, `video_access`.`views`, `home_tasks`.`ht_status` ,  `video_access`.`video_id`,
+    `ht_user`.`ht_user_ballov_p1`, `ht_user`.`ht_user_maxballov_p1`, `ht_user`.`ht_user_status_p1`, `ht_user`.`ht_user_date_p1`, 
+    `ht_user`.`ht_user_ballov_p2`, `ht_user`.`ht_user_maxballov_p2`, `ht_user`.`ht_user_status_p2`, `ht_user`.`ht_user_date_p2` 
+    FROM  `home_tasks`
+        LEFT JOIN `ht_user` on `home_tasks`.`ht_number`=`ht_user`.`ht_number` AND `ht_user`.`user_id` =  :student_id
+        LEFT JOIN `videos` on `videos`.`video_lesson_num`=`home_tasks`.`ht_number`
+        LEFT JOIN `video_access` ON `video_access`.`video_id`=`videos`.`video_id` AND `video_access`.`user_vk_id`= :student_id
+    WHERE  `home_tasks`.`ht_status`
+    IN ('Выполнение',  'Проверка',  'Завершено')
+    ORDER BY  `for_order`, `home_tasks`.`ht_number` DESC;
 ") or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (2)');
 
 $stmt->execute([
@@ -101,28 +95,28 @@ $stmt->execute([
 ]) or $out->make_wrong_resp('Ошибка базы данных: выполнение запроса (2)');
 
 while ($homeTasksRequest = $stmt->fetch(PDO::FETCH_ASSOC)) {
-	if((int)$homeTasksRequest['access'] == 1 || $student['user_type'] == "Куратор") $prosmotrov = $homeTasksRequest['views'];
-	else $prosmotrov = 'недост.';
-	
-	if($student['user_type'] == 'Интенсив') {
-		if($homeTasksRequest['ht_num'] >= 100 && $homeTasksRequest['ht_num'] < 200) $prosmotrov = $homeTasksRequest['views'];
-			else continue;
-	}
+    if((int)$homeTasksRequest['access'] == 1 || $student['user_type'] == "Куратор") $prosmotrov = $homeTasksRequest['views'];
+    else $prosmotrov = 'недост.';
+
+    if($student['user_type'] == 'Интенсив') {
+        if($homeTasksRequest['ht_num'] >= 100 && $homeTasksRequest['ht_num'] < 200) $prosmotrov = $homeTasksRequest['views'];
+            else continue;
+    }
     $homeTasks[] = [
-        'htNum' => (string)$daysData['ht_num'],
-        'videoId' => (string)$daysData['video_id'],
-        'forOrder' => (string)$daysData['for_order'],
-        'access' => (string)$daysData['access'],
+        'htNum' => (string)$homeTasksRequest['ht_num'],
+        'videoId' => (string)$homeTasksRequest['video_id'],
+        'forOrder' => (string)$homeTasksRequest['for_order'],
+        'access' => (string)$homeTasksRequest['access'],
         'views' => (string)$prosmotrov,
-        'htStatus' => (string)$daysData['ht_status'],
-        'htUserBallovP1' => (string)$daysData['ht_user_ballov_p1'],
-        'htUserMaxballovP1' => (string)$daysData['ht_user_maxballov_p1'],
-        'htUserStatusP1' => (string)$daysData['ht_user_status_p1'],
-        'htUserDateP1' => (string)$daysData['ht_user_date_p1'],
-        'htUserBallovP2' => (string)$daysData['ht_user_ballov_p2'],
-        'htUserMaxballovP2' => (string)$daysData['ht_user_maxballov_p2'],
-        'htUserStatusP2' => (string)$daysData['ht_user_status_p2'],
-        'htUserDateP2' => (string)$daysData['ht_user_date_p2']
+        'htStatus' => (string)$homeTasksRequest['ht_status'],
+        'htUserBallovP1' => (string)$homeTasksRequest['ht_user_ballov_p1'],
+        'htUserMaxballovP1' => (string)$homeTasksRequest['ht_user_maxballov_p1'],
+        'htUserStatusP1' => (string)$homeTasksRequest['ht_user_status_p1'],
+        'htUserDateP1' => (string)$homeTasksRequest['ht_user_date_p1'],
+        'htUserBallovP2' => (string)$homeTasksRequest['ht_user_ballov_p2'],
+        'htUserMaxballovP2' => (string)$homeTasksRequest['ht_user_maxballov_p2'],
+        'htUserStatusP2' => (string)$homeTasksRequest['ht_user_status_p2'],
+        'htUserDateP2' => (string)$homeTasksRequest['ht_user_date_p2']
     ];
 }
 $stmt->closeCursor();
