a3e05165560f72a7eab7da83051f066aeabbea4d
Stepan <130833594+HoneyMan123@users.noreply.github.com>
2024-10-19 16:47:43 +0700

    improved completion

--- php/user_info/get_user_zachets_auto.php
+++ php/user_info/get_user_zachets_auto.php
@@ -1,4 +1,4 @@
-<?php // Получение данных вопроса для редактирования (ч.1)
+<?php // "Получение автоматических зачётов пользователя"
 header('Content-Type: application/json; charset=utf-8');
 
 require $_SERVER['DOCUMENT_ROOT'] . '/app/api/includes/config.inc.php';
@@ -6,16 +6,15 @@ require $_SERVER['DOCUMENT_ROOT'] . '/app/api/includes/root_classes.inc.php';
 
 
 // класс запроса
-class TrainP1AdminGetQuestion extends MainRequestClass {
-    public $questionId = ''; // идентификатор вопроса ч.1, данные которого нужно получить
+class UserInfoGetUserZachetsAuto extends MainRequestClass {
+     // идентификатор вопроса ч.1, данные которого нужно получить
 }
 $link=mysqli_init();
-
-$in = new TrainP1AdminGetQuestion();
+$in = new UserInfoGetUserZachetsAuto();
 $in->from_json(file_get_contents('php://input'));
 
 // класс ответа
-class TrainP1AdminGetQuestionResp extends MainResponseClass {
+class UserInfoGetUserZachetsAutoResp extends MainResponseClass {
     /*
      * Словарь, который имеет следующие поля:
      *     - qId - идентификатор вопроса
@@ -25,20 +24,18 @@ class TrainP1AdminGetQuestionResp extends MainResponseClass {
      *     - isPublished - опубликован ли (0/1)
      *     - isSelfmade - сделанный ли самостоятельно (0/1)
      */
-    public $question = []; // данные вопроса
+    public $zachets = []; // данные зачета, взятые из дб  
 
-    public $chapters = []; // доступные разделы для вопроса (массив строк)
 }
-$out = new TrainP1AdminGetQuestionResp();
+$out = new UserInfoGetUserZachetsAutoResp();
 
 //--------------------------------Подключение к базе данных
 
 try {
     // Use PDO for the database connection
     $pdo = new PDO("mysql:host=localhost;port=3366;dbname=task;charset=utf8mb4", 'root', 'zj8qz1sd6ruhqq24xaxlwq');
-} catch(PDOException $e) {
-    echo json_encode(["error" => "Connection failed: " . $e->getMessage()]);
-    exit;
+} catch(PDOException $exception) {
+    $out->make_wrong_resp('Нет соединения с базой данных');
 }
 
 // Check user access 
@@ -70,24 +67,37 @@ $fetchedQuery = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if ($user_type =='Куратор' && $fetchedQuery[0]['user_curator']  != $in->userId) exit('это не твой ученик');
 
 $query="SELECT `zachets_auto`.*, `zachet_user`.`zu_status`, `zachet_user`.`zu_popitka`, `zachet_user`.`zu_errors`, `zachet_user`.`zu_errors`  FROM `zachets_auto` 
-LEFT JOIN `zachet_user` ON `zachet_user`.`zachet_id`=`zachets_auto`.`za_id` AND `zachet_user`.`user_id`='".$student_id."'
+LEFT JOIN `zachet_user` ON `zachet_user`.`zachet_id`=`zachets_auto`.`za_id` AND `zachet_user`.`user_id`=:student_id
 WHERE `za_showed`=1
 ORDER BY `za_id` DESC;";
 
+$stmt = $pdo->prepare($query) or $out->make_wrong_resp('Ошибка базы данных: подготовка запроса (1)');;
+$stmt->execute([':student_id' => $student_id])  or $out->make_wrong_resp('Ошибка базы данных: выполнение запроса (1)') ;
+$zachets = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
+foreach ($zachets as $zachet) {
+    $out->zachets[0] = [
+        'zaId' => (string) $zachet['za_id'],
+        'zaDateStart' => (string) $zachet['za_date_start'],
+        'zaDeadline' => (string) $zachet['za_deadline'],
+        'zaMaxTime' => (string) $zachet['za_max_time'],
+        'zaMaxPopitok' => (string) $zachet['za_max_popitok'],
+        'zaQuestions' => (string) $zachet['za_questions'],
+        'zaMaxErrors' => (string) $zachet['za_max_errors'],
+        'zaLessonNumbers' => (string) $zachet['za_lesson_numbers'],
+        'zaShowed' => (string) $zachet['za_showed'],
+        'zuStatus' => (string) $zachet['zu_status'],
+        'zuPopitka' => (string) $zachet['zu_popitka'],
+        // ...
+    ];
+}
 
-$stmt = $pdo->prepare($query);
-$stmt->execute();
-
-$fetchedQuery = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-// Now extract the user_curator or handle potential empty results
-
-
-
-
-$out->zachets =  $fetchedQuery;
+    
 $out->make_resp('');
+
+
+
+
+
 
